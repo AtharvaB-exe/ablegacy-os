1cb1c1388d2661571e7736bfdb397c323f4a2037
Atharva <atharvabulbule02@gmail.com>
2026-02-02 19:42:46 +0530

    Fix theme provider types

--- src/components/theme-provider.tsx
+++ src/components/theme-provider.tsx
@@ -2,8 +2,12 @@
 
 import * as React from "react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
-import { type ThemeProviderProps } from "next-themes/dist/types";
 
-export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
+// We use 'React.ComponentProps' instead of importing specific types
+// This prevents the "Cannot find module" error on Vercel
+export function ThemeProvider({
+  children,
+  ...props
+}: React.ComponentProps<typeof NextThemesProvider>) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
 }
